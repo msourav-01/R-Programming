2699c9622039ecc7cd8c65384222c646b458ffdb
msourav-01 <msourav01@gmail.com>
2015-08-12 18:45:40 +0530

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,24 +1,30 @@
 pollutantmean <- function(directory, pollutant, id){
-                    for(i in id)
-                    {   
-                        if((floor(log10(i))+1) == 1) {
-                         filename <- paste("00",i,".csv", sep="")
-                          
-                        } else if((floor(log10(i))+1) == 2) {
-                        filename <- paste("0",i,".csv", sep="")
-                        }
-                        else if((floor(log10(i))+1) == 3) {
-                          filename <- paste(i,".csv", sep="") 
-                        }
-                        
-                      if(!exists("df")){
-                      df <- read.csv(filename, header=T)
-                      }else if (exists("df")){
-                        df_temp <- read.csv(filename,header=T)
-                        df<-rbind(df,df_temp)
-                        rm(df_temp)
-                      }
-                    }
-  #mean(df[pollutant],na.rm=T)
-  df
+  a <- 0
+  s<- 0
+  for(i in id)
+  { 
+    if((floor(log10(i))+1) == 1) {
+      filename <- paste("00",i,".csv", sep="")
+      
+    } else if((floor(log10(i))+1) == 2) {
+      filename <- paste("0",i,".csv", sep="")
+    }
+    else if((floor(log10(i))+1) == 3) {
+      filename <- paste(i,".csv", sep="") 
+    }
+    if(a == 0){
+      
+      df <- read.csv(filename, header=T)
+      a=a+1
+    } else if(a != 0){
+      
+      df_temp <- read.csv(filename, header=T)
+      df <- rbind(df,df_temp)
+    }
+    
+    s<-mean(df[,pollutant],na.rm=T)
+    
+    
+    s
+  }
 }
